ce6dc423d282ba8f27cb93bdfe76a812275e4127
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2025-04-28 23:54:20 +0100

    add menu

--- Machine-Management-Ltd.c
+++ Machine-Management-Ltd.c
@@ -4,5 +4,25 @@
 
 void main()
 {
-	printf("test"); 
+	int choice = 0; 
+
+	printf("\n=========================================\n");
+	printf("   MACHINERY MANAGEMENT FLEET SYSTEM\n");
+	printf("=========================================\n");
+
+	do {
+        printf("\nMAIN MENU:\n");
+        printf("1) Add machine\n");
+        printf("2) Display all machines to screen\n");
+        printf("3) Display machine details\n");
+        printf("4) Update a machine's details\n");
+        printf("5) Delete machine\n");
+        printf("6) Generate statistics\n");
+        printf("7) Print all machine details into a report file\n");
+        printf("8) List all the machinery in order of current valuation\n");
+        printf("0) Exit\n");
+        printf("\nEnter your choice: ");
+
+        scanf("%d", &choice);
+    } while (choice != 0); 
 }
